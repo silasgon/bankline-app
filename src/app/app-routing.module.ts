03e140db975790adeed3b88f97810c1700f56c75
Silas Gon <silasidc@gmail.com>
2022-05-24 18:37:24 -0300

    conectando a api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
+
+const routes: Routes = [
+
+  { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
+  { path: 'movimentacoes', component: MovimentacaoListComponent },
+  { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
 
 @Component({
   selector: 'app-movimentacao-list',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovimentacaoListComponent implements OnInit {
 
-  constructor() { }
+  movimentacoes: any;
+
+  constructor(private movimentacaoService: MovimentacaoService) { }
 
   ngOnInit(): void {
+    this.listMovimentacoes();
+  }
+  listMovimentacoes(): void {
+    this.movimentacaoService.list()
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
